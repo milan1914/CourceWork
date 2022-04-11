d00ea2c06754eaa3496879df7141159aad3b7549
Milan.Zhekov <milanzhekov@gmail.com>
2022-04-11 16:04:40 +0300

    final adjustments

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,12 +1,14 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+
 import java.time.Duration;
 
 public class ProductsPage {
@@ -14,6 +16,7 @@ public class ProductsPage {
     //constant
     private static final String ADD_TO_CART_LOCATOR = "//button[@id='add-to-cart-sauce-labs-%s']";
     private static final String REMOVE_FROM_CART_LOCATOR = "//button[@id='remove-sauce-labs-%s']";
+    private static final String IMAGE_SCR = "//img[@src='/static/media/%s']";
 
     @FindBy(className = "shopping_cart_link")
     private WebElement shoppingCartLink;
@@ -52,5 +55,12 @@ public class ProductsPage {
     //this method leads to the cart
     public void enterIntoCart() {
         shoppingCartLink.click();
+    }
+
+    //this method check if the correct image is displayed
+    public boolean checkImage (String imageName) throws NoSuchElementException {
+        String xpathOfImage = String.format(IMAGE_SCR, imageName);
+        WebElement productImage = driver.findElement(By.xpath(xpathOfImage));
+        return productImage.isDisplayed();
     }
 }
---
+++ src/test/java/qa/automation/ProblemUserTest.java
@@ -0,0 +1,17 @@
+package qa.automation;
+
+import base.TestUtil;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+public class ProblemUserTest extends TestUtil {
+
+    @Test
+    public void verifyImage() {
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productsPage = loginPage.login("problem_user", "secret_sauce");
+        Assert.assertTrue(productsPage.checkImage("bolt-shirt-1200x1500.c0dae290.jpg"));
+    }
+}
